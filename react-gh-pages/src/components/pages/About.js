86ded5324688f489051a59490c5e9e2d396654ae
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-08-26 13:57:52 -0400

    updating

--- react-gh-pages/src/components/pages/About.js
+++ react-gh-pages/src/components/pages/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Col, Image } from 'react-bootstrap';
-import profilePic from '../../images/profile-pic.jpg';
+import profilePic from '../../images/profile-pic.png';
 
 const p = {
     textIndent: '12px',
